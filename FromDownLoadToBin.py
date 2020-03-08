a4dcd032f32bfe86298ecdfec258e9ac44db4937
andrea cortesi <37798519+MrCorte@users.noreply.github.com>
2020-03-08 11:33:59 +0100

    Add files via upload

---
+++ FromDownLoadToBin.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import datetime
+import time
+oggi= datetime.datetime.now()
+
+for (dirpath, dirnames, filenames) in os.walk('/Users/andrea/Downloads/'):
+    oggi= datetime.datetime.now()
+    diff = oggi - datetime.datetime.fromtimestamp(os.path.getatime(dirpath))
+    if(diff > datetime.timedelta(days=7)):
+        shutil.rmtree(dirpath)
+        print("file eliminato :",filenames)
+    else:
+        print("*")
+
+
